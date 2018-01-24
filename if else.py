b68db1418e74cae13f9b74559c0f3999c8a2129a
Ahmedrajiv <35676568+Ahmedrajiv@users.noreply.github.com>
2018-01-24 10:25:07 +0530

    Add files via upload

---
+++ if else.py
@@ -0,0 +1,5 @@
+a= int (input("Enter the value to check whether it is +ve or -ve"))
+if a>0:
+    print("+ve")
+else :
+    print("-ve")
---
+++ p3.py
@@ -0,0 +1,12 @@
+print("Rajiv Uddin Ahmed")
+5/2
+a= int (input("Enter the value to check whether it is +ve or -ve"))
+if a > 0:
+      print("The given number is +ve")
+if a < 0:
+      print("The given number is -ve")
+
+
+
+
+                     
