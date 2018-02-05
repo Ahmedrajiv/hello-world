d6d7e6d855249510232953af3741c891914c050d
Ahmedrajiv <35676568+Ahmedrajiv@users.noreply.github.com>
2018-02-05 10:26:22 +0530

    Add files via upload

---
+++ DatatypesS4.py
@@ -0,0 +1,46 @@
+import math
+a=1
+b=2
+a,b= b,a+b
+print (a,b)
+
+a= 10,20,30
+print (a)
+
+a=10
+b=20
+c=a%b
+c=complex(a,b)
+print (c)
+
+a= "Rajiv Uddin Ahmed"
+print (a[0:5])
+print (a[6:8])
+#print (a[0:4])
+#print (a[0:7])
+
+a= [10,"Hello",10.80,100]
+print (a[1])
+
+l= ['a','b','c','d','e']
+print(l[1])
+print(l[0:5])
+#print(l[5])
+
+tup1 = ('Rajiv', 'Uddin', 'Ahmed')
+tup2 = (1, 2, 3, 4, 5, 6, 7 )
+print ("tup1[0]: " ,tup1[0])
+print ("tup2[1:5]: " ,tup2[1:5])
+
+a=4
+print(math.sqrt(a))
+
+d={1:"Rajiv", "A":30}
+print ("d[1]:", d[1])
+print ("d['A']:", d['A'])
+
+set = {1,2,2,4}
+print (set) 
+       
+
+
